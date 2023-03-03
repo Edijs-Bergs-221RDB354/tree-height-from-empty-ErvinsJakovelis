16f4a026ff11ffed582b6d6864f694fc5955e568
Ervīns Jakovelis <125136553+ErvinsJakovelis@users.noreply.github.com>
2023-03-03 11:25:56 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -2,26 +2,52 @@
 
 import sys
 import threading
+import numpy as np
 
 
 def compute_height(n, parents):
     # Write this function
+    vertiba=0
     max_height = 0
+    height = np.zeros(n)
+    apskatitie =np.zeros(n)
+    for i in range(n):
+        apskatitie[vertiba] = 1
+        print(vertiba)
+        print(apskatitie)
+        if height[vertiba] != 0:
+            return np.count_nonzero(apskatitie)
+        height[vertiba] = 1
+        vertiba = parents[vertiba]
+        if i!=0:
+            for m in range (n):
+                if (m != vertiba and height[m] !=0):
+                    height[m] +=1
+        
+
     # Your code here
-    return max_height
+    return apskatitie 
 
 
 def main():
     # implement input form keyboard and from files
-    
+    ievade = input()
+    if 'F' in ievade:
+        path = input()
+        path = "test/" + path
+        if 'a' not in path:
+            with open(path, "r") as f:
+                    n = int(f.readline())
+                    parents = np.array(list(map(int, f.readline().split())))
+                    print(compute_height(n, parents))
+    if 'I' in ievade:
+        n = int(input())
+        parents = np.array(list(map(int, input().split())))
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
-    
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-
-
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
